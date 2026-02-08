6b9d9fc8a47e284425b44c92b94b2805035c6044
Miza <mizaeron@gmail.com>
2026-02-08 11:48:05 +0000

    Change how the ships are created and stored

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,6 +1,9 @@
 import { Ship } from "./ship";
 
 export class Gameboard {
+  constructor() {
+    this.storedShip = null;
+  }
   static rowLetters = "ABCDEFGHIJ".split("");
   static board = new Map(
     Gameboard.rowLetters.map((l) => [
@@ -9,26 +12,36 @@ export class Gameboard {
     ]),
   );
 
-  placeShip(ship) {
-    switch (ship.length) {
-      case 1:
-        Gameboard.board.get(ship.coords[0][0])[ship.coords[0][1]] = "X";
-        break;
-      case 2:
-        Gameboard.board.get(ship.coords[0][0])[ship.coords[0][1]] = "X";
-        Gameboard.board.get(ship.coords[1][0])[ship.coords[1][1]] = "X";
-        break;
-      case 3:
-        Gameboard.board.get(ship.coords[0][0])[ship.coords[0][1]] = "X";
-        Gameboard.board.get(ship.coords[1][0])[ship.coords[1][1]] = "X";
-        Gameboard.board.get(ship.coords[2][0])[ship.coords[2][1]] = "X";
-        break;
-      case 4:
-        Gameboard.board.get(ship.coords[0][0])[ship.coords[0][1]] = "X";
-        Gameboard.board.get(ship.coords[1][0])[ship.coords[1][1]] = "X";
-        Gameboard.board.get(ship.coords[2][0])[ship.coords[2][1]] = "X";
-        Gameboard.board.get(ship.coords[3][0])[ship.coords[3][1]] = "X";
-        break;
-    }
+  createShip(length) {
+    const ship = new Ship(length);
+    // switch (ship.length) {
+    //   case 1:
+    //     Gameboard.board.get(ship.coords[0][0])[ship.coords[0][1]] = "X";
+    //     break;
+    //   case 2:
+    //     Gameboard.board.get(ship.coords[0][0])[ship.coords[0][1]] = "X";
+    //     Gameboard.board.get(ship.coords[1][0])[ship.coords[1][1]] = "X";
+    //     break;
+    //   case 3:
+    //     Gameboard.board.get(ship.coords[0][0])[ship.coords[0][1]] = "X";
+    //     Gameboard.board.get(ship.coords[1][0])[ship.coords[1][1]] = "X";
+    //     Gameboard.board.get(ship.coords[2][0])[ship.coords[2][1]] = "X";
+    //     break;
+    //   case 4:
+    //     Gameboard.board.get(ship.coords[0][0])[ship.coords[0][1]] = "X";
+    //     Gameboard.board.get(ship.coords[1][0])[ship.coords[1][1]] = "X";
+    //     Gameboard.board.get(ship.coords[2][0])[ship.coords[2][1]] = "X";
+    //     Gameboard.board.get(ship.coords[3][0])[ship.coords[3][1]] = "X";
+    //     break;
+    // }
+
+    this.storedShip = ship;
+    return this.storedShip;
   }
+
+  // receiveAttack(H, V) {
+  //   if (Gameboard.board.get(H)[V] === "X") {
+  //     // return placeShip();
+  //   }
+  // }
 }
--- src/test.js
+++ src/test.js
@@ -35,31 +35,61 @@ test("Show created coords of the ship", () => {
   expect(ship2.coords).toEqual([["B", 4]]);
 });
 
-test("Place ship on the board", () => {
-  const ship = new Ship(1);
+test("Create ship from within gameboard class", () => {
   const board = new Gameboard();
-  ship.assignCoords("A", 3);
-  board.placeShip(ship);
-  expect(Gameboard.board.get(ship.coords[0][0])[ship.coords[0][1]]).toEqual(
-    "X",
-  );
-  const ship2 = new Ship(4);
-  ship2.assignCoords("B", 3);
-  ship2.assignCoords("B", 4);
-  ship2.assignCoords("B", 5);
-  ship2.assignCoords("B", 6);
-
-  board.placeShip(ship2);
-  expect(Gameboard.board.get(ship2.coords[0][0])[ship2.coords[0][1]]).toEqual(
-    "X",
-  );
-  expect(Gameboard.board.get(ship2.coords[1][0])[ship2.coords[1][1]]).toEqual(
-    "X",
-  );
-  expect(Gameboard.board.get(ship2.coords[2][0])[ship2.coords[2][1]]).toEqual(
-    "X",
-  );
-  expect(Gameboard.board.get(ship2.coords[3][0])[ship2.coords[3][1]]).toEqual(
-    "X",
-  );
+  board.createShip(2);
+  expect(board.createShip(2)).toBe(board.storedShip);
+  board.createShip(3);
+  expect(board.createShip(3)).toBe(board.storedShip);
 });
+
+test("Create a ship and assign coords", () => {
+  const board = new Gameboard();
+  board.createShip(3);
+  board.storedShip.assignCoords("A", 3);
+  board.storedShip.assignCoords("A", 4);
+  board.storedShip.assignCoords("A", 5);
+
+  expect(board.storedShip.coords).toEqual([
+    ["A", 3],
+    ["A", 4],
+    ["A", 5],
+  ]);
+});
+
+// test("Place ship on the board", () => {
+//   const board = new Gameboard();
+//   board.placeShip(2);
+//   expect(Gameboard.board.get(ship.coords[0][0])[ship.coords[0][1]]).toEqual(
+//     "X",
+//   );
+// const ship2 = new Ship(4);
+// ship2.assignCoords("B", 3);
+// ship2.assignCoords("B", 4);
+// ship2.assignCoords("B", 5);
+// ship2.assignCoords("B", 6);
+
+// board.placeShip(ship2);
+// expect(Gameboard.board.get(ship2.coords[0][0])[ship2.coords[0][1]]).toEqual(
+//   "X",
+// );
+// expect(Gameboard.board.get(ship2.coords[1][0])[ship2.coords[1][1]]).toEqual(
+//   "X",
+// );
+// expect(Gameboard.board.get(ship2.coords[2][0])[ship2.coords[2][1]]).toEqual(
+//   "X",
+// );
+// expect(Gameboard.board.get(ship2.coords[3][0])[ship2.coords[3][1]]).toEqual(
+//   "X",
+// );
+// });
+
+// test("Get the correct ship on the board", () => {
+//   const board = new Gameboard();
+//   const ship1 = new Ship(2);
+//   ship1.assignCoords("A", 2);
+//   ship1.assignCoords("A", 3);
+//   board.placeShip(ship1);
+//   console.log(board.placeShip(ship1));
+//   expect(board.receiveAttack("A", 2)).toEqual(ship1);
+// });
